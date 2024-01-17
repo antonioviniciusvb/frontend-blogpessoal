d979ded0fe2a3ae48752a34bc9def236dfdd3bb2
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2024-01-17 14:46:22 -0300

    :sparkles: feat: Home: NavBar, body e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import Footer from './pages/footer/Footer'
 import Home from './pages/home/Home'
+import NavBar from './pages/navBar/NavBar'
 
 function App() {
 
   return (
     <>
+      <NavBar />
       <Home />
+      <Footer />
     </>
   )
 }
